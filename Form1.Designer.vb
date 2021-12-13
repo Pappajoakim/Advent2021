2fdd2f74ddd123d1b950566a687a93e5895b0708
Pappajoakim <69308708+Pappajoakim@users.noreply.github.com>
2021-12-13 13:14:20 +0100

    Add project files.

---
+++ Form1.Designer.vb
@@ -0,0 +1,108 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(66, 33)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Button1"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(66, 89)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(29, 15)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Svar"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(66, 152)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(41, 15)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = "Label2"
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(66, 63)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(290, 23)
+        Me.TextBox1.TabIndex = 3
+        Me.TextBox1.Text = "J:\Program\Adventofcode\2021\input12.txt"
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(66, 188)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 23)
+        Me.Button2.TabIndex = 4
+        Me.Button2.Text = "Avsluta"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(66, 107)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(100, 23)
+        Me.TextBox2.TabIndex = 5
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Button2 As Button
+    Friend WithEvents TextBox2 As TextBox
+End Class
---
+++ Form1.vb
@@ -0,0 +1,355 @@
+﻿Public Class Form1
+    Public Datan(25, 1) As Integer
+    Public D(1, 1) As Integer
+    Public CheckBig(25, 1) As String
+    Public AntalRätt As Integer
+    Public AntalFlash As Integer
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim FileName As String
+        Dim TextRad As String
+        TextBox1.Text = "J:\Program\Adventofcode\2021\input3.txt"
+        FileName = TextBox1.Text
+        TextBox2.Text = ""
+        Dim AntalRader As Integer
+        Dim Rad As Integer
+        Dim Kolumn As Integer
+        Dim Bang As Integer
+
+        AntalRader = 0
+        AntalRätt = 0
+        AntalFlash = 0
+        Rad = 0
+        Kolumn = 0
+        Bang = 0
+
+        ' antal rader i texten
+        AntalRader = System.IO.File.ReadAllLines(FileName).Length
+        ReDim D(AntalRader, 13)
+        Dim ObjReader As New System.IO.StreamReader(FileName)
+        Do While ObjReader.Peek() <> -1
+            TextRad = ObjReader.ReadLine()
+            For I = 0 To 11
+                D(Rad, I) = Mid(TextRad, I + 1, 1)
+            Next
+            Rad = Rad + 1
+        Loop
+
+
+        For I = 0 To 11
+            TextBox2.Text = TextBox2.Text + summera(I)
+        Next
+        Stop
+        'TextBox2.Text = Convert.ToInt32(TextBox2.Text)
+
+        Label2.Text = "Klart"
+    End Sub
+
+    Function summera(ByVal X As Integer) As String
+        Dim Noll As Integer
+        Dim Ett As Integer
+        Noll = 0
+        Ett = 0
+        For I = 0 To 1000
+            If D(I, X) = "0" Then
+                Ett = Ett + 1
+            Else
+                Noll = Noll + 1
+            End If
+        Next
+        'Stop
+        If Ett > Noll Then
+            summera = "1"
+        Else
+            summera = "0"
+        End If
+        'summera = Str(Ett) + "," + Str(Noll)
+    End Function
+
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        End
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Button1.PerformClick()
+    End Sub
+
+    Function AddOne()
+        For I = 0 To 9
+            For X = 0 To 9
+                Datan(I, X) = Datan(I, X) + 1
+            Next X
+        Next I
+
+    End Function
+
+    Function Nolla()
+        For I = 0 To 9
+            For X = 0 To 9
+                If Datan(I, X) < 0 Then
+                    Datan(I, X) = 0
+                    AntalFlash = AntalFlash + 1
+                End If
+            Next X
+        Next I
+    End Function
+    Function Blast()
+        For Ra = 0 To 9
+            For Ko = 0 To 9
+                If Datan(Ra, Ko) > 9 Then
+                    Select Case Ra
+                        Case 0
+                            If Ko = 0 Then
+                                Datan(Ra + 1, Ko) = Datan(Ra + 1, Ko) + 1
+                                Datan(Ra + 1, Ko + 1) = Datan(Ra + 1, Ko + 1) + 1
+                                Datan(Ra, Ko + 1) = Datan(Ra, Ko + 1) + 1
+                            ElseIf Ko = 9 Then
+                                Datan(Ra + 1, Ko) = Datan(Ra + 1, Ko) + 1
+                                Datan(Ra + 1, Ko - 1) = Datan(Ra + 1, Ko - 1) + 1
+                                Datan(Ra, Ko - 1) = Datan(Ra, Ko - 1) + 1
+                            Else
+                                Datan(Ra + 1, Ko) = Datan(Ra + 1, Ko) + 1
+                                Datan(Ra + 1, Ko + 1) = Datan(Ra + 1, Ko + 1) + 1
+                                Datan(Ra + 1, Ko - 1) = Datan(Ra + 1, Ko - 1) + 1
+                                Datan(Ra, Ko + 1) = Datan(Ra, Ko + 1) + 1
+                                Datan(Ra, Ko - 1) = Datan(Ra, Ko - 1) + 1
+                            End If
+                        Case 9
+                            If Ko = 0 Then
+                                Datan(Ra - 1, Ko) = Datan(Ra - 1, Ko) + 1
+                                Datan(Ra - 1, Ko + 1) = Datan(Ra - 1, Ko + 1) + 1
+                                Datan(Ra, Ko + 1) = Datan(Ra, Ko + 1) + 1
+                            ElseIf Ko = 9 Then
+                                Datan(Ra - 1, Ko) = Datan(Ra - 1, Ko) + 1
+                                Datan(Ra - 1, Ko - 1) = Datan(Ra - 1, Ko - 1) + 1
+                                Datan(Ra, Ko - 1) = Datan(Ra, Ko - 1) + 1
+                            Else
+                                Datan(Ra - 1, Ko) = Datan(Ra - 1, Ko) + 1
+                                Datan(Ra - 1, Ko + 1) = Datan(Ra - 1, Ko + 1) + 1
+                                Datan(Ra - 1, Ko - 1) = Datan(Ra - 1, Ko - 1) + 1
+                                Datan(Ra, Ko + 1) = Datan(Ra, Ko + 1) + 1
+                                Datan(Ra, Ko - 1) = Datan(Ra, Ko - 1) + 1
+                            End If
+                        Case Else
+                            If Ko = 0 Then
+                                Datan(Ra - 1, Ko) = Datan(Ra - 1, Ko) + 1
+                                Datan(Ra - 1, Ko + 1) = Datan(Ra - 1, Ko + 1) + 1
+                                Datan(Ra, Ko + 1) = Datan(Ra, Ko + 1) + 1
+                                Datan(Ra + 1, Ko) = Datan(Ra + 1, Ko) + 1
+                                Datan(Ra + 1, Ko + 1) = Datan(Ra + 1, Ko + 1) + 1
+                            ElseIf Ko = 9 Then
+                                Datan(Ra - 1, Ko) = Datan(Ra - 1, Ko) + 1
+                                Datan(Ra - 1, Ko - 1) = Datan(Ra - 1, Ko - 1) + 1
+                                Datan(Ra, Ko - 1) = Datan(Ra, Ko - 1) + 1
+                                Datan(Ra + 1, Ko) = Datan(Ra + 1, Ko) + 1
+                                Datan(Ra + 1, Ko - 1) = Datan(Ra + 1, Ko - 1) + 1
+                            Else
+                                Datan(Ra - 1, Ko) = Datan(Ra - 1, Ko) + 1
+                                Datan(Ra - 1, Ko + 1) = Datan(Ra - 1, Ko + 1) + 1
+                                Datan(Ra - 1, Ko - 1) = Datan(Ra - 1, Ko - 1) + 1
+                                Datan(Ra, Ko + 1) = Datan(Ra, Ko + 1) + 1
+                                Datan(Ra, Ko - 1) = Datan(Ra, Ko - 1) + 1
+                                Datan(Ra + 1, Ko) = Datan(Ra + 1, Ko) + 1
+                                Datan(Ra + 1, Ko + 1) = Datan(Ra + 1, Ko + 1) + 1
+                                Datan(Ra + 1, Ko - 1) = Datan(Ra + 1, Ko - 1) + 1
+                            End If
+                    End Select
+                    Datan(Ra, Ko) = -100
+                    AntalRätt = AntalRätt + 1
+                    Call Blast()
+                End If
+            Next
+        Next
+    End Function
+
+
+
+
+    Function Day1() As Object
+        Dim TextLine As String
+        Dim FileName As String
+        FileName = TextBox1.Text
+        Dim AntalRader As Integer
+        Dim Depth1 As Integer
+        Dim Depth2 As Integer
+        Dim Depth3 As Integer
+        Dim AntalRätt As Integer
+        Dim Deeper As Integer
+        Dim PrevDepth As Integer
+        AntalRader = 0
+        AntalRätt = 0
+        Deeper = 0
+        Label1.Text = 0
+        Dim ObjReader As New System.IO.StreamReader(FileName)
+        Do While ObjReader.Peek() <> -1
+            Depth3 = ObjReader.ReadLine()
+            AntalRader = AntalRader + 1
+        Loop
+        Dim Depth(AntalRader) As Integer
+        AntalRader = 0
+        Dim ObjReader2 As New System.IO.StreamReader(FileName)
+        Do While ObjReader2.Peek() <> -1
+            Depth(AntalRader) = ObjReader2.ReadLine()
+            AntalRader = AntalRader + 1
+        Loop
+        For I = 0 To 1997
+            Depth1 = Depth(I) + Depth(I + 1) + Depth(I + 2)
+            Depth2 = Depth(I + 1) + Depth(I + 2) + Depth(I + 3)
+            If Depth1 < Depth2 Then
+                AntalRätt = AntalRätt + 1
+            End If
+            If I = 1996 Then Stop
+        Next
+
+
+
+        Label1.Text = AntalRätt
+        Label2.Text = "Klart"
+
+
+    End Function
+    Function Day2() As Object
+        Dim FileName As String
+        FileName = TextBox1.Text
+        Dim AntalRader As Integer
+        Dim ForW As Integer
+        Dim Aim As Integer
+        Dim Depth3 As Integer
+        Dim AntalRätt As Integer
+        AntalRader = 0
+        AntalRätt = 0
+        ForW = 0
+        Aim = 0
+        Depth3 = 0
+        ' sätter in texten i en array
+        Dim ObjReader As New System.IO.StreamReader(FileName)
+        Do While ObjReader.Peek() <> -1
+            ObjReader.ReadLine()
+            AntalRader = AntalRader + 1
+        Loop
+        Dim Depth(AntalRader) As String
+        AntalRader = 0
+        Dim ObjReader2 As New System.IO.StreamReader(FileName)
+        Do While ObjReader2.Peek() <> -1
+            Depth(AntalRader) = ObjReader2.ReadLine()
+            AntalRader = AntalRader + 1
+        Loop
+
+
+        For I = 0 To Depth.Length - 2
+            Select Case Strings.Left(Depth(I), 1)
+                Case "f"
+                    ForW = ForW + Strings.Right(Depth(I), 1)
+                    Depth3 = Depth3 + (Strings.Right(Depth(I), 1) * Aim)
+                Case "d"
+                    Aim = Aim + Strings.Right(Depth(I), 1)
+                Case "u"
+                    Aim = Aim - Strings.Right(Depth(I), 1)
+                Case Else
+                    MsgBox("fel värde")
+
+            End Select
+            'If I = 998 Then Stop
+
+        Next
+
+
+
+        TextBox2.Text = ForW * Depth3
+        Label2.Text = "Klart"
+    End Function
+    Function day11()
+        Dim FileName As String
+        Dim TextRad As String
+        FileName = TextBox1.Text
+        Dim AntalRader As Integer
+        Dim Rad As Integer
+        Dim Kolumn As Integer
+        Dim Bang As Integer
+
+        AntalRader = 0
+        AntalRätt = 0
+        AntalFlash = 0
+        Rad = 0
+        Kolumn = 0
+        Bang = 0
+        ' sätter in texten i en array
+        Dim ObjReader As New System.IO.StreamReader(FileName)
+        Do While ObjReader.Peek() <> -1
+            ObjReader.ReadLine()
+            AntalRader = AntalRader + 1
+        Loop
+        AntalRader = 0
+        Dim ObjReader2 As New System.IO.StreamReader(FileName)
+
+        For I = 0 To 9
+            TextRad = ObjReader2.ReadLine()
+            For X = 0 To 9
+                Datan(I, X) = Mid(TextRad, X + 1, 1)
+            Next X
+        Next I
+
+
+        For I = 1 To 1000
+            'Stop
+            Call Function() AddOne()
+            Call Blast()
+            Call Nolla()
+            If AntalFlash = 100 Then
+                MsgBox(I)
+                Stop
+            Else
+                AntalFlash = 0
+            End If
+        Next
+
+        TextBox2.Text = AntalRätt
+        Label2.Text = "Klart"
+    End Function
+
+    Function day12()
+        Dim FileName As String
+        Dim TextRad As String
+        FileName = TextBox1.Text
+        Dim AntalRader As Integer
+        Dim Rad As Integer
+        Dim Kolumn As Integer
+        Dim Bang As Integer
+
+        AntalRader = 0
+        AntalRätt = 0
+        AntalFlash = 0
+        Rad = 0
+        Kolumn = 0
+        Bang = 0
+
+        ' sätter in texten i en array
+        Dim ObjReader As New System.IO.StreamReader(FileName)
+        Do While ObjReader.Peek() <> -1
+            TextRad = ObjReader.ReadLine()
+            AntalRader = AntalRader + 1
+            Bang = InStr(TextRad, "-")
+            Datan(AntalRader - 1, 0) = Strings.Left(TextRad, Bang - 1)
+            Datan(AntalRader - 1, 1) = Strings.Right(TextRad, Strings.Len(TextRad) - Bang)
+        Loop
+        ' checking vilka som kan besökas flera gånger.
+        Stop
+        For I = 0 To 25
+            If Char.IsUpper(Strings.Chr(Strings.Asc(Datan(I, 0)))) = True Then
+                CheckBig(I, 0) = "Y"
+            Else
+                CheckBig(I, 0) = "N"
+            End If
+            If Char.IsUpper(Strings.Chr(Strings.Asc(Datan(I, 1)))) = True Then
+                CheckBig(I, 1) = "Y"
+            Else
+                CheckBig(I, 1) = "N"
+            End If
+        Next
+
+
+
+        TextBox2.Text = AntalRätt
+        Label2.Text = "Klart"
+    End Function
+End Class
